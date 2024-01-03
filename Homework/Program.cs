a4f13bfe109b2e6158b111a67af55e61c2b6b5b0
sulton <global-sultonmalik326@gmail.com>
2024-01-03 19:23:58 +0500

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Fayl nomini kiriting: ");
+        string fileName = Console.ReadLine();
+
+        string fullPath = FindFilePath(fileName);
+
+        if (fullPath != null)
+        {
+            Console.WriteLine($"Fayl manzili: {fullPath}");
+        }
+        else
+        {
+            Console.WriteLine("Bunday nomli fayl topilmadi.");
+        }
+    }
+
+    static string FindFilePath(string fileName)
+    {
+      
+        string rootPath = @"C:\";
+
+       
+        string[] files = Directory.GetFiles(rootPath, fileName, SearchOption.AllDirectories);
+
+        if (files.Length > 0)
+        {
+           
+            return files[0];
+        }
+        else
+        {
+            
+            return null;
+        }
+    }
+}
